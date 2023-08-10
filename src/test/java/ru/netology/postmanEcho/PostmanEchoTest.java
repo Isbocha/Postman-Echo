0260dacbbc54045728497df6c44269b66c0598f3
InnaB <isbocha@gmail.com>
2023-08-10 23:19:25 +0300

    Add test commit

---
+++ src/test/java/ru/netology/postmanEcho/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.postmanEcho;
+
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.CDATASection;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnDemoAccaunt() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
